ffda045367a12c86eac6c67ecf041437595d78b7
unknown <rohit.phulpagar98@gmail.com>
2023-10-22 16:50:40 +0530

    changes are done

--- server.js
+++ server.js
@@ -58,5 +58,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`server is running on port: ${PORT}`);
+  console.log(`server is running on port: ${process.env.PORT}`);
 });
